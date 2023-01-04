e541b7ccf5e0cd1cddfe9e6e80bf8fa7be3da0a0
Yulia Miller <miller82.98@mail.ru>
2023-01-03 21:44:04 -0800

    Добавьте файлы проекта.

---
+++ 6.2.2. constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mynamespase
+{
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        //Конструктор без параметров
+        public Pen()
+        {
+            color = "black";
+            cost = 100;
+        }
+
+        //Конструктор с параметрами
+        public Pen(string penColor, int penCost)
+        {
+            color= penColor;
+            cost = penCost;
+        }
+    }
+}
